01d36ff51e1c5dfa89a5f6d29a9a5989ea233964
Dante Vargas <dntv17@outlook.com>
2024-12-05 00:01:30 -0700

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import Home from './components/home';
 import Services from './components/services';
 
@@ -7,8 +8,34 @@ function App() {
     <div className="min-h-screen bg-gray-50">
       <Services />
       
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> 97d7414 (Initialize project using Create React App)
     </div>
   );
 }
 
+<<<<<<< HEAD
 export default App;
+=======
+export default App;
+>>>>>>> 97d7414 (Initialize project using Create React App)
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import ReactDOM from 'react-dom/client'; // Use createRoot for React 18
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import React Router
 import './index.css'; // Import global styles
@@ -18,3 +19,23 @@ root.render(
     </Router>
   </React.StrictMode>
 );
+=======
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+>>>>>>> 97d7414 (Initialize project using Create React App)
