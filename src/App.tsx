b1f6f346e65ed166cd3b2b34df33a2c225ad8082
Dante Vargas <dntv17@outlook.com>
2024-12-13 23:00:59 -0700

    Update .gitignore to exclude build directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import Home from './components/home';
 import Services from './components/services';
 
@@ -8,34 +7,8 @@ function App() {
     <div className="min-h-screen bg-gray-50">
       <Services />
       
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> 97d7414 (Initialize project using Create React App)
     </div>
   );
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> 97d7414 (Initialize project using Create React App)
